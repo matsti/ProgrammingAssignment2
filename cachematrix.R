8918e592bd1b6fb136a46b26a166d147c6a107bd
matsti <mats@springam.co.uk>
2015-05-18 16:48:23 +0100

    sumitting assignment 2 with additional comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,37 @@
 ## Matrix inversion using a cache to avoid recalculation
 
 ## makeCacheMatrix creates an object containing a matrix and its inverse
-## once the inverse has been calculated
+## once the inverse has been calculated using the cashSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        # object variables creation and initialisation
         m <-x
         im <- NULL
         
+        # list of the methods implemented in the makeCacheMatrix object
         list(
-                set = function(x) {
+                set = function(x) {  # allows to set a new matrix value, the inverse is set to null
                         m <<- x
                         im <<- NULL
                         },
-                get = function() m,
+                get = function() m,  # return the value of the matrix
                 setinverse = function(x) im <<- x, # should only be called from cacheSolve
-                getinverse = function() im
+                getinverse = function() im  # returns the value of the inverse
          )
 }
 
 
-## cacheSolve returns the inverse matrix, from cache if it has already calculated
-## it or computes it and stores it into a cache
+## cacheSolve returns the inverse matrix. The inverse is returned from cache if it has already
+## been calculated. If not, the inverse is calculated and stored into a cache.
 
 cacheSolve <- function(x, ...) {
 
-        xim <- x$getinverse()
-        if(!is.null(xim)) {
-                message("getting cached data")
+        xim <- x$getinverse()  # checking for a non-null inverse
+        if(!is.null(xim)) {  # if an inverse exists, returning the cached matrix with message
+                message("getting cached matrix data")
                 return(xim)
         }
-        x$setinverse(solve(x$get(), ...))
+        x$setinverse(solve(x$get(), ...))  # calculating the inverse using solve and updating the 
+        ## value of the inverse in the makeCacheMatrix object
 }
